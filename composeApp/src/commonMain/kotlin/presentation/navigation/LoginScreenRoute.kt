13bbadfd912accc4f0db8e9696384096532d446d
Steve <steve@kitkat@gmail.com>
2024-07-24 21:26:32 +0700

    Can navigate to the register screen

--- composeApp/src/commonMain/kotlin/presentation/navigation/LoginScreenRoute.kt
+++ composeApp/src/commonMain/kotlin/presentation/navigation/LoginScreenRoute.kt
@@ -5,8 +5,10 @@ package presentation.navigation
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
 import co.touchlab.kermit.Logger
 import org.koin.compose.viewmodel.koinViewModel
+import presentation.authentication.login.LoginAction
 import presentation.authentication.login.LoginEvent
 import presentation.authentication.login.LoginScreen
 import presentation.authentication.login.LoginViewModel
@@ -18,11 +20,19 @@ data object LoginScreenRoute : Screen {
     override fun Content() {
         val loginViewModel = koinViewModel<LoginViewModel>()
         val loginState = loginViewModel.loginState
+        val navigator = LocalNavigator.current
 
         LoginScreen(
             loginState = loginState,
             onLoginAction = { loginAction ->
-                loginViewModel.onLoginAction(loginAction)
+                when(loginAction) {
+                    LoginAction.OnSignUpClicked -> {
+                        navigator?.push(item = RegisterScreenRoot)
+                    }
+                    else -> {
+                        loginViewModel.onLoginAction(loginAction)
+                    }
+                }
             }
         )
 
