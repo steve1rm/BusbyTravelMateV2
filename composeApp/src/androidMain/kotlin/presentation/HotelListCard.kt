679ac58b4b50ef08f41bce765fd34704609f7a9a
Steve <steve@kitkat@gmail.com>
2024-08-14 23:35:03 +0700

    Adding flowrow for the offers

--- composeApp/src/androidMain/kotlin/presentation/HotelListCard.kt
+++ composeApp/src/androidMain/kotlin/presentation/HotelListCard.kt
@@ -2,18 +2,29 @@ package presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -21,7 +32,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import me.androidbox.busbytravelmatev2.R
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import presentation.designsystem.ui.theme.BusbyTravelMateTheme
+import kotlin.math.max
 
 @Composable
 fun HotelListCard(
@@ -30,8 +43,68 @@ fun HotelListCard(
 
 }
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun ImageCard(
+fun HotelDetailCard(
+    modifier: Modifier = Modifier
+) {
+    var maxWidth by remember {
+        mutableIntStateOf(0)
+    }
+
+    val maxWidthDp = with(LocalDensity.current) {
+        maxWidth.toDp()
+    }
+
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        FlowRow(modifier = Modifier,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)) {
+
+            Text(modifier = Modifier
+                .defaultMinSize(minWidth = maxWidthDp)
+                .onSizeChanged { size ->
+                    maxWidth = max(maxWidth, size.width)
+                },
+                text = "Breakfast Included", fontSize = 12.sp)
+
+            Text(
+                modifier = Modifier
+                    .defaultMinSize(minWidth = maxWidthDp)
+                    .onSizeChanged { size ->
+                        maxWidth = max(maxWidth, size.width)
+                    },
+                text = "Free Cancellation", fontSize = 12.sp)
+
+            Text(
+                modifier = Modifier
+                    .defaultMinSize(minWidth = maxWidthDp)
+                    .onSizeChanged { size ->
+                        maxWidth = max(maxWidth, size.width)
+                    },
+                text = "Preferred Room", fontSize = 12.sp)
+            Text(modifier = Modifier
+                .defaultMinSize(minWidth = maxWidthDp)
+                .onSizeChanged { size ->
+                    maxWidth = max(maxWidth, size.width)
+                },
+                text = "Exclusive Offers", fontSize = 12.sp)
+
+            Text(
+                modifier = Modifier
+                    .defaultMinSize(minWidth = maxWidthDp)
+                    .onSizeChanged { size ->
+                        maxWidth = max(maxWidth, size.width)
+                    },
+                text = "Discounts Demand", fontSize = 12.sp)
+        }
+    }
+}
+
+@Composable
+fun HotelImageCard(
     modifier: Modifier = Modifier
 ) {
     Box(modifier = Modifier.fillMaxWidth().padding(bottom = 22.dp)) {
@@ -103,6 +176,7 @@ fun ImageCard(
 @Composable
 fun PreviewImageCard() {
     BusbyTravelMateTheme {
-        ImageCard()
+      //  HotelImageCard()
+        HotelDetailCard()
     }
 }
