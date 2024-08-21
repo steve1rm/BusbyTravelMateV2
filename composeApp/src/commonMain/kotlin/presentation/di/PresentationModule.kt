975b983acb78aa2e7625b5e74123df171736606f
Steve <steve@kitkat@gmail.com>
2024-08-21 19:59:40 +0700

    Fixing issue of wasm not being able to inject the Login view model

--- composeApp/src/commonMain/kotlin/presentation/di/PresentationModule.kt
+++ composeApp/src/commonMain/kotlin/presentation/di/PresentationModule.kt
@@ -1,11 +1,30 @@
 package presentation.di
 
-import org.koin.compose.viewmodel.dsl.viewModelOf
+
+import domain.authentication.UserEmailPasswordValidator
+import domain.authentication.usecases.LoginUserWithPasswordUseCase
+import domain.authentication.usecases.RegisterUserUseCase
 import org.koin.dsl.module
 import presentation.authentication.login.LoginViewModel
 import presentation.authentication.signup.RegisterViewModel
 
 val presentationModule = module {
+/*
     viewModelOf(::LoginViewModel)
     viewModelOf(::RegisterViewModel)
+*/
+
+    single {
+        LoginViewModel(
+         //   get<UserEmailPasswordValidator>(),
+            get<LoginUserWithPasswordUseCase>()
+        )
+    }
+
+    single {
+        RegisterViewModel(
+            get<UserEmailPasswordValidator>(),
+            get<RegisterUserUseCase>()
+        )
+    }
 }
