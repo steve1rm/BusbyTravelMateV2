801985ab3b5eb05d88dbb3376a43b6750a4590f9
Steve <steve@kitkat@gmail.com>
2024-08-30 22:56:32 +0700

    fixes dependency with DesktopPlatform setting for preferences

--- composeApp/src/desktopMain/kotlin/di/DesktopSpecificModule.kt
+++ composeApp/src/desktopMain/kotlin/di/DesktopSpecificModule.kt
@@ -1,10 +1,24 @@
 package di
 
+import com.russhwolf.settings.PreferencesSettings
+import com.russhwolf.settings.Settings
+import data.authentication.local.AuthorizationLocalDataSource
+import data.authentication.local.imp.AuthorizationLocalDataSourceImp
 import org.koin.dsl.module
 import presentation.designsystem.components.ShowMessageManager
 import presentation.designsystem.components.ShowMessageManagerImp
+import java.util.prefs.Preferences
 
 val desktopSpecificModule = module {
+    single<Settings> {
+        val preferences = Preferences.userRoot()
+
+        PreferencesSettings(preferences)
+    }
+
+    factory<AuthorizationLocalDataSource> {
+        AuthorizationLocalDataSourceImp(get<Settings>())
+    }
 
     factory<ShowMessageManager> {
         ShowMessageManagerImp()
