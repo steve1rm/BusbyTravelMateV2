21f8ce75e7e382611fc8e95666b4a08fb7ebf4ab
Steve <steve@kitkat@gmail.com>
2024-08-30 16:36:39 +0700

    Adding desktop message pop

--- composeApp/src/desktopMain/kotlin/di/DesktopSpecificModule.kt
+++ composeApp/src/desktopMain/kotlin/di/DesktopSpecificModule.kt
@@ -1,7 +1,12 @@
 package di
 
 import org.koin.dsl.module
+import presentation.designsystem.components.ShowMessageManager
+import presentation.designsystem.components.ShowMessageManagerImp
 
 val desktopSpecificModule = module {
 
+    factory<ShowMessageManager> {
+        ShowMessageManagerImp()
+    }
 }
--- composeApp/src/desktopMain/kotlin/presentation/designsystem/components/PlatformUI.desktop.kt
+++ composeApp/src/desktopMain/kotlin/presentation/designsystem/components/PlatformUI.desktop.kt
@@ -1,9 +1,34 @@
 package presentation.designsystem.components
 
-actual class ShowMessageManagerImp actual constructor() : ShowMessageManager {
+import java.awt.*
+import javax.swing.*
+
+class ToastMessage(message: String, duration: Int) : JWindow() {
+    init {
+        val label = JLabel(message)
+        label.isOpaque = true
+        label.background = Color.BLACK
+        label.foreground = Color.WHITE
+        label.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+        add(label)
+        pack()
+        setLocationRelativeTo(null)
+        isVisible = true
+
+        Timer(duration) { dispose() }.start()
+    }
+}
+
+actual class ShowMessageManagerImp : ShowMessageManager {
     actual override fun showMessage(
         message: String,
         duration: Duration
     ) {
+        val toastDuration = when (duration) {
+            Duration.SHORT_DURATION -> 4
+            Duration.LONG_DURATION -> 8
+        }
+
+        ToastMessage(message, toastDuration)
     }
 }
