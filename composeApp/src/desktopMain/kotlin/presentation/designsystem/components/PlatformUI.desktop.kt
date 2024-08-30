e1168de5a8f56d95b3589d33cd87cabe8b019ef8
Steve <steve@kitkat@gmail.com>
2024-08-30 23:14:58 +0700

    Can show a popup on desktop for incorrect credentials

--- composeApp/src/desktopMain/kotlin/presentation/designsystem/components/PlatformUI.desktop.kt
+++ composeApp/src/desktopMain/kotlin/presentation/designsystem/components/PlatformUI.desktop.kt
@@ -1,23 +1,12 @@
 package presentation.designsystem.components
 
-import java.awt.*
-import javax.swing.*
-
-class ToastMessage(message: String, duration: Int) : JWindow() {
-    init {
-        val label = JLabel(message)
-        label.isOpaque = true
-        label.background = Color.BLACK
-        label.foreground = Color.WHITE
-        label.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
-        add(label)
-        pack()
-        setLocationRelativeTo(null)
-        isVisible = true
-
-        Timer(duration) { dispose() }.start()
-    }
-}
+import java.awt.Color
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import javax.swing.BorderFactory
+import javax.swing.JLabel
+import javax.swing.JWindow
+import javax.swing.Timer
 
 actual class ShowMessageManagerImp : ShowMessageManager {
     actual override fun showMessage(
@@ -30,5 +19,35 @@ actual class ShowMessageManagerImp : ShowMessageManager {
         }
 
         ToastMessage(message, toastDuration)
+    }
+}
+
+class ToastMessage(message: String, duration: Int) : JWindow() {
+    init {
+        val label = JLabel(message)
+        label.isOpaque = true
+        label.background = Color.BLACK
+        label.foreground = Color.WHITE
+        label.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+        add(label)
+
+        pack()
+        setLocationRelativeTo(null)
+        isVisible = true
+
+        // Add a mouse listener to hide the toast on click
+        addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent) {
+                dispose()
+            }
+        })
+
+        // Use a timer to dispose the toast after the specified duration
+        val timer = Timer(duration * 1000) {
+            dispose()
+        }
+
+        timer.isRepeats = false
+        timer.start()
     }
 }
