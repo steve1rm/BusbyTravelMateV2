29904716605a5a1dbbd9bbe9d40167f4917e4a07
Steve <steve@kitkat@gmail.com>
2024-08-31 21:08:18 +0700

    Can show the popup dialog for incorrect password for wasm

--- composeApp/src/wasmJsMain/kotlin/presentation/designsystem/components/PlatformUI.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/presentation/designsystem/components/PlatformUI.wasmJs.kt
@@ -1,10 +1,13 @@
 package presentation.designsystem.components
 
+import kotlinx.browser.window
+
 actual class ShowMessageManagerImp: ShowMessageManager {
     actual override fun showMessage(
         message: String,
         duration: Duration
     ) {
-
+        // show popup box here
+        window.alert(message)
     }
 }
